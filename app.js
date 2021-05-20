ff2a7ae83886f97180cc27ca40373f939bad3f82
MykeSelivanov <michael777selivanov@gmail.com>
2021-05-20 00:03:30 -0500

    gif-search

---
+++ app.js
@@ -0,0 +1,28 @@
+// Require Libraries
+const express = require('express');
+
+// App Setup
+const app = express();
+
+// Middleware
+const exphbs  = require('express-handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Routes
+app.get('/', (req, res) => {
+    console.log(req.query);
+    res.render('home');
+  });
+
+app.get('/greetings/:name', (req, res) => {
+    // grab the name from the path provided
+    const name = req.params.name;
+    // render the greetings view, passing along the name
+    res.render('greetings', { name });
+})
+
+// Start Server
+app.listen(3000, () => {
+  console.log('Gif Search listening on port localhost:3000!');
+});
